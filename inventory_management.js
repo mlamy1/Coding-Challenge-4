631e743444a10bd2dbe7e2fedd4286897d3b20e5
Myia Lamy <mlamy@usf.edu>
2024-09-14 21:28:10 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,11 @@
+// Task 1: Create an Inventory Array of Product Objects
+
+let inventory = [
+    {name: 'Camera Strap', price: 500, quantity: 5, lowStockLevel: 2 },
+    {name: 'Camera Lens', price: 1000, quantity: 10, lowStockLevel: 4 },
+    {name: 'Camera Bag', price: 1500, quantity: 15, lowStockLevel: 6 },
+    {name: 'Camera Film', price: 2000, quantity: 20, lowStockLevel: 8 },
+    {name: 'Camera', price: 2500, quantity: 25, lowStockLevel: 10 },
+]; 
+
+// Array contains product: name, price, quanity, and lowstock level of the 5 example products listed. 
