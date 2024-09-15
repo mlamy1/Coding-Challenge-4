55de8362add68e3faf48179f6966bb2f248c3487
Myia Lamy <mlamy@usf.edu>
2024-09-14 23:08:52 -0400

    Create displayProductDetails function

--- inventory_management.js
+++ inventory_management.js
@@ -9,3 +9,14 @@ let inventory = [
 ]; 
 
 // Array contains product: name, price, quanity, and lowstock level of the 5 example products listed. 
+
+// Task 2: Create a Function to Display Product Details
+
+function displayProductDetails(inventory) {
+    const stockStatus = inventory.quantity <= inventory.lowStockLevel ? "Low Stock" : "In Stock" // Use ternary operator to check stock status.
+    return `${inventory.name} is ${stockStatus}` // Use return function to display if the product is in stock or not. 
+        
+} 
+console.log(displayProductDetails(inventory[0])); // Used to display which product is in stock. 
+ console.log(inventory[0]); // Used to display the product's: name, price, quanity, and lowstocklevel.
+
